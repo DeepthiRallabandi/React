e410e23543477210a1523c03b8744228a4b34525
DeepthiRallabandi <deepthirallabandi3@gmail.com>
2024-12-15 13:09:44 +0530

    completed reacttest

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Table from './components/Table'
+function App() {
+  return (
+  <div>
+    <Table/>
+    <img src="src/assets/training.jpg" height="200" width="200"/>
+   </div>
+  )
+}
+export default App
---
+++ src/components/tablebody.jsx
@@ -0,0 +1,13 @@
+function TableBody(){
+    return(
+        <div>
+            <tbody>
+                <tr>
+                    <td>Deepthi(btech)</td>
+                    <td>Student</td>
+                </tr>
+            </tbody>
+        </div>
+    )
+}
+export default TableBody;
